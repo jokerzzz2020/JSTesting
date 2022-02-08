1d3ced7e111162ff138e7ff8a50ac94be44fd33c
MJ Chen <acejokerrr@gmail.com>
2022-02-09 00:27:00 +0800

    modify

--- test1.js
+++ test1.js
@@ -6,7 +6,7 @@ function fibonacci(index) {
 
     fib[0] = 0;
     fib[1] = 1;
-    for (i = 2; i <= 10; i++) {
+    for (i = 2; i <= index; i++) {
         fib[i] = fib[i - 2] + fib[i - 1];
         console.log(fib[i]);
     }
